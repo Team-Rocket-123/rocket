b2fbf3bd66c5262118da57c1747c0374158a899b
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-01-31 00:15:11 -0700

    creating merge conflict for Joe

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,5 +3,5 @@ package com.example.lab4;
 public abstract class Shape {
     int x;
     int y;
-    String color = "this is williams color pink";
+    String color = "this is the main branch color click your branch for the merge conflict";
 }
