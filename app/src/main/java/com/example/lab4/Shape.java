7a85a7b3776bff3583ec976d92a85b39b099d980
Hattero <danielkhai2004@gmail.com>
2026-01-31 00:39:19 -0700

    making conflict

--- app/src/main/java/com/example/lab4/Shape.java
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -3,5 +3,5 @@ package com.example.lab4;
 public abstract class Shape {
     int x;
     int y;
-    String color = "this is the main branch color click your branch for the merge conflict";
+    String color = "this is the main branch color click your branch for the merge conflict" ;
 }
